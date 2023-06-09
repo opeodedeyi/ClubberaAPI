d0115c4aef458704daaa74892568d44a1f2b8891
Odedeyi Opeyemi <opeyemiodedeyi@gmail.com>
2023-06-10 00:32:56 +0100

    Added all parts

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,31 +1,22 @@
-// Import the Mongoose library
 const mongoose = require('mongoose');
-// Load environment variables from the .env file
 require('dotenv').config();
 
-// Get the MongoDB connection URI from the environment variables
 const URI = process.env.MONGODB_ATLAS_URI;
-
-// Set strictQuery to false for the Mongoose connection
 mongoose.set('strictQuery', false);
 
-// Connect to MongoDB using the URI and specified options
 mongoose.connect(URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-// Log a message when Mongoose successfully connects to the database
 mongoose.connection.on('connected', () => {
   console.log('Mongoose successfully connected to the database.');
 });
 
-// Log an error message if there's a connection error
 mongoose.connection.on('error', (err) => {
   console.error(`Mongoose connection error: ${err}`);
 });
 
-// Log a message when Mongoose disconnects from the database
 mongoose.connection.on('disconnected', () => {
   console.log('Mongoose disconnected from the database.');
 });
